dd6bc071eb383c84e5d8c1a83f409b74f4ddd647
notThisMoment <1911259514@qq.com>
2021-06-16 19:40:49 +0800

    first

--- STL/rb_tree.cpp
+++ STL/rb_tree.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "rb_tree.h"
+#include <cstddef>
 
 typedef bool __rb_tree_color_type;
 const __rb_tree_color_type __rb_tree_red = false;
@@ -24,8 +25,52 @@ struct __rb_tree_node_base
         return x;
     }
 
+    static base_ptr maximum(base_ptr x)
+    {
+        while (x -> right != 0) x = x->right;
+        return x;
+    }
+
 };
+
+template <class Value>
+struct __rb_tree_node : public _-__rb_tree_node_base
+{
+    typedef __rb_tree_node<Value>* link_type;
+    Value value_field;      // 节点值
+};
+
+
+// 下面是一些迭代器的设计
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
 
 struct __rb_tree_base_iterator
 {
+    typedef __rb_tree_node_base::base_ptr base_ptr;
+    typedef bidirectional_iterator_tag iterator_category;
+    typedef ptrdiff_t difference_type;
+
+    base_ptr node;          // 用来与容器之间产生一个连结关系
+
+    void increment()
+    {
+        if (node->right != 0)       // 如果有右子节点。状况（1）
+        {
+            node = node->right;     // 就向右走
+            while (node->left != 0) // 然后一直往左子树走到底
+                node = node->left;  // 就是结果
+        }
+        else                        // 没有右子节点。状况（2）
+        {
+            base_ptr y = node->parent;
+            while (node == y->right)
+            {
+                node = y;
+                y = y->parent;
+            }
+        }
+    }
 };
